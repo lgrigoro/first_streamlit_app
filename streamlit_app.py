5bb831d15ad47030789288d879fa0bab69cd8a0f
lgrigoro <lukagrygor@gmail.com>
2022-08-08 23:14:01 +0200

    Update streamlit_app.py
    added some content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+strealit.header('Breakfast menu')
+
+strealit.text('Omega 3 & Blueberry Oatmeal')
+strealit.text('Kale, Spinach &| Rocket Smoothie')
+strealit.text('Hard-Boiled Free-Range Egg')
