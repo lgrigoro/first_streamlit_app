199d8008e63fc153c188684f73268170942fd124
lgrigoro <lukagrygor@gmail.com>
2022-08-09 16:55:30 +0200

    Update streamlit_app.py
    starting parametrizing the API link

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,11 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvie api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) #just writes data to the screen
 
 #take the JSON version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-#output it on the screen as a table
+#output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
