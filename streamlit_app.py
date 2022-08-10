026a728a9c57542af63a377ac6e203ebb4521f15
lgrigoro <lukagrygor@gmail.com>
2022-08-10 13:03:29 +0200

    Update streamlit_app.py
    fruit addition fix #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if streamlit.button('Get Fruit Load List'):
   
   
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
   
   
 # Allow the end user to add a fruit to the list
